03456b04b686f0e2a541c1d1ff3180bc61a033ee
Yashdeep Shetty <yashdeep@clevertap.com>
2023-03-11 19:51:09 +0530

    Updated script for the timestamp calculation

--- scripts/delete_branches_older_commits.sh
+++ scripts/delete_branches_older_commits.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 echo "Starting script"
-git status
-git checkout main
-git pull origin main
-merged_branches=$(git branch -r --merged | grep -v "main$")
+max_difference_days=90
+merged_branches=$(git checkout main | git pull | git branch  -r --merged | grep -v "main$")
 for branch in ${merged_branches}
 do
     echo "Checking latest commit timestamp for $branch :"
     latest_commit_id=$(git log $branch | grep commit | head -1 | awk -F ' ' '{print $2}')
     echo "Latest commit_id for $branch is $latest_commit_id"
+    latest_commit_timestamp=$(git show --format="%aI" $latest_commit_id | head -1)
+    latest_commit_timestamp=$(date -u -d "$latest_commit_timestamp" +"%Y-%m-%dT%H:%M:%SZ")
+    current_timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    echo $latest_commit_timestamp
+    echo $current_timestamp
+    latest_commit_timestamp_in_seconds=$(date -d $latest_commit_timestamp +%s)
+    current_timestamp_in_seconds=$(date -d $current_timestamp +%s)
+    difference_in_seconds=$(($current_timestamp_in_seconds-$latest_commit_timestamp_in_seconds))
+    difference_in_days=$(($difference_in_seconds/60/60/24))
+    if [ $difference_in_days -ge $max_difference_days ]
+    then
+        echo "Deleting $branch" 
+        git push origin --delete $branch
+        if [$? -ne 0]
+        then
+            echo "Failed to delete $branch"
+        else
+            echo "Successfully deleted $branch"
+        fi
+    else
+        echo "Skipped $branch since latest-commit:$latest_commit_id is not greater than $max_difference_days days"
+    fi
 done
