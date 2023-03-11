1742a97fafc58d4411249743cea4d5e20f9e9396
Yashdeep Shetty <yashdeep@clevertap.com>
2023-03-11 19:53:34 +0530

    Updated script for the timestamp calculation string

--- scripts/delete_branches_older_commits.sh
+++ scripts/delete_branches_older_commits.sh
@@ -27,6 +27,6 @@ do
             echo "Successfully deleted $branch"
         fi
     else
-        echo "Skipped $branch since latest-commit:$latest_commit_id is not greater than $max_difference_days days (currently just $difference_in_days behind today)"
+        echo "Skipped $branch since latest-commit:$latest_commit_id is not greater than $max_difference_days days (currently just $difference_in_days days behind today)"
     fi
 done
