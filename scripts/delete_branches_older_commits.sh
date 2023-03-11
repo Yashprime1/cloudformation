de72e87e7363c3638543ae5bc830a3647da8cca6
Yashdeep Shetty <yashdeep@clevertap.com>
2023-03-11 18:15:46 +0530

    Updated script for timestamp

--- scripts/delete_branches_older_commits.sh
+++ scripts/delete_branches_older_commits.sh
@@ -7,7 +7,6 @@ merged_branches=$(git branch -r --merged | grep -v "main$")
 for branch in ${merged_branches}
 do
     echo "Checking latest commit timestamp for $branch :"
-    latest_commit_id=$(git log $branch | grep commit | head -1)
+    latest_commit_id=$(git log $branch | grep commit | head -1 | awk -F ' ' '{print $2}')
     echo "Latest commit_id for $branch is $latest_commit_id"
-    
 done
